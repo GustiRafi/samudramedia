9703ebfdf82b728e72148fcd9bf10853594c6a58
GustiRafi <gustirafi49@gmail.com>
2023-07-23 18:30:34 +0700

    change analytic color

--- resources/views/admin/administator.blade.php
+++ resources/views/admin/administator.blade.php
@@ -2,6 +2,7 @@
 @section('content')
 <div class="container-fluid px-4">
     <h1 class="mt-4">Dashboard</h1>
+    <h3 id="jam" class="my-2"></h3>
     <ol class="breadcrumb mb-4">
         <li class="breadcrumb-item active">Welcome back {{ auth()->user()->name }}</li>
     </ol>
@@ -12,7 +13,7 @@
                 <div class="py-4">
                     <h4><i class="fas fa-book"></i></h4>
                     <h5>Book</h5>
-                    <h6 class="">{{$book}}</h6>
+                    <h6 class="">{{ $book }}</h6>
                 </div>
             </div>
         </div>
@@ -21,7 +22,7 @@
                 <div class="py-4">
                     <h4><i class="fa-solid fa-bell-concierge"></i></h4>
                     <h5>Service</h5>
-                    <h6 class="">{{$service}}</h6>
+                    <h6 class="">{{ $service }}</h6>
                 </div>
             </div>
         </div>
@@ -30,7 +31,7 @@
                 <div class="py-4">
                     <h4><i class="fa-solid fa-book"></i></h4>
                     <h5>Journal</h5>
-                    <h6 class="">{{$journal}}</h6>
+                    <h6 class="">{{ $journal }}</h6>
                 </div>
             </div>
         </div>
@@ -39,7 +40,7 @@
                 <div class="py-4">
                     <h4><i class="fas fa-envelope"></i></h4>
                     <h5>Email</h5>
-                    <h6 class="">{{$inbox}}</h6>
+                    <h6 class="">{{ $inbox }}</h6>
                 </div>
             </div>
         </div>
@@ -48,26 +49,26 @@
         <div class="col-md-6 col-12">
             <div class="card">
                 <div class="card-header d-flex justify-content-between">
-                   <div class="card-header">
-                      <h4 class="card-title">Pertumbuhan Buku Minggu Ini</h4>
-                   </div>
+                    <div class="card-header">
+                        <h4 class="card-title">Pertumbuhan Buku Minggu Ini</h4>
+                    </div>
                 </div>
                 <div class="card-body">
-                   <div id="pertumbuhan-buku"></div>
+                    <div id="pertumbuhan-buku"></div>
                 </div>
-             </div>
+            </div>
         </div>
         <div class="col-md-6 col-12">
             <div class="card">
                 <div class="card-header d-flex justify-content-between">
-                   <div class="card-header">
-                      <h4 class="card-title">Email / Pertanayaan Minggu Ini</h4>
-                   </div>
+                    <div class="card-header">
+                        <h4 class="card-title">Email / Pertanayaan Minggu Ini</h4>
+                    </div>
                 </div>
                 <div class="card-body">
-                   <div id="pertumbuhan-email"></div>
+                    <div id="pertumbuhan-email"></div>
                 </div>
-             </div>
+            </div>
         </div>
     </div>
 </div>
@@ -75,45 +76,71 @@
 </div>
 @endsection
 @section('js')
-    <script>
-        new Morris.Line({
-            // ID of the element in which to draw the chart.
-            element: 'pertumbuhan-buku',
-            // Chart data records -- each entry in this array corresponds to a point on
-            // the chart.
-            data: JSON.parse(`{!! json_encode($bookAnalytic) !!}`),
-            // The name of the data record attribute that contains x-values.
-            xkey: 'date',
-            // A list of names of data record attributes that contain y-values.
-            ykeys: ['value'],
-            xLabelFormat: function (d) {
-                return ("0" + d.getDate()).slice(-2) + '-' + ("0" + (d.getMonth() + 1)).slice(-2) + '-' + d.getFullYear();
-            },
-            gridTextSize: 10,
-            // Labels for the ykeys -- will be displayed when you hover over the
-            // chart.
-            labels: ['Book'],
-            lineColors: ['#868611']
-        });
+<script>
+    function updateJam() {
+        // Mendapatkan waktu saat ini
+        const waktuSekarang = new Date();
 
-        new Morris.Line({
-            // ID of the element in which to draw the chart.
-            element: 'pertumbuhan-email',
-            // Chart data records -- each entry in this array corresponds to a point on
-            // the chart.
-            data: JSON.parse(`{!! json_encode($inboxAnalytic) !!}`),
-            // The name of the data record attribute that contains x-values.
-            xkey: 'date',
-            // A list of names of data record attributes that contain y-values.
-            ykeys: ['terkirim','dibalas'],
-            xLabelFormat: function (d) {
-                return ("0" + d.getDate()).slice(-2) + '-' + ("0" + (d.getMonth() + 1)).slice(-2) + '-' + d.getFullYear();
-            },
-            gridTextSize: 10,
-            // Labels for the ykeys -- will be displayed when you hover over the
-            // chart.
-            labels: ['email belum dijawab','email dijawab'],
-            lineColors: ['#868611']
-        });
-    </script>
+        // Mendapatkan jam, menit, dan detik dari waktu saat ini
+        const jam = waktuSekarang.getHours();
+        const menit = waktuSekarang.getMinutes();
+        const detik = waktuSekarang.getSeconds();
+
+        // Format jam, menit, dan detik agar selalu memiliki 2 digit angka (misalnya: 09:05:02)
+        const jamFormat = String(jam).padStart(2, '0');
+        const menitFormat = String(menit).padStart(2, '0');
+        const detikFormat = String(detik).padStart(2, '0');
+
+        // Menampilkan waktu jam dalam elemen dengan format: "jam:menit:detik"
+        $('#jam').text(`${jamFormat}:${menitFormat}:${detikFormat}`);
+    }
+
+    $(document).ready(function () {
+        updateJam(); // Panggil fungsi ini pertama kali agar jam langsung muncul saat halaman dimuat.
+        setInterval(updateJam, 1000); // Memanggil fungsi updateJam setiap detik (1000ms)
+    });
+
+    new Morris.Line({
+        // ID of the element in which to draw the chart.
+        element: 'pertumbuhan-buku',
+        // Chart data records -- each entry in this array corresponds to a point on
+        // the chart.
+        data: JSON.parse(`{!! json_encode($bookAnalytic) !!}`),
+        // The name of the data record attribute that contains x-values.
+        xkey: 'date',
+        // A list of names of data record attributes that contain y-values.
+        ykeys: ['value'],
+        xLabelFormat: function (d) {
+            return ("0" + d.getDate()).slice(-2) + '-' + ("0" + (d.getMonth() + 1)).slice(-2) + '-' + d
+                .getFullYear();
+        },
+        gridTextSize: 10,
+        // Labels for the ykeys -- will be displayed when you hover over the
+        // chart.
+        labels: ['Book'],
+        lineColors: ['#0040e5']
+    });
+
+    new Morris.Line({
+        // ID of the element in which to draw the chart.
+        element: 'pertumbuhan-email',
+        // Chart data records -- each entry in this array corresponds to a point on
+        // the chart.
+        data: JSON.parse(`{!! json_encode($inboxAnalytic) !!}`),
+        // The name of the data record attribute that contains x-values.
+        xkey: 'date',
+        // A list of names of data record attributes that contain y-values.
+        ykeys: ['terkirim', 'dibalas'],
+        xLabelFormat: function (d) {
+            return ("0" + d.getDate()).slice(-2) + '-' + ("0" + (d.getMonth() + 1)).slice(-2) + '-' + d
+                .getFullYear();
+        },
+        gridTextSize: 10,
+        // Labels for the ykeys -- will be displayed when you hover over the
+        // chart.
+        labels: ['email belum dijawab', 'email dijawab'],
+        lineColors: ['#0040e5', '#dc3545']
+    });
+
+</script>
 @endsection
